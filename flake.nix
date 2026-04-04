c8bb4925857c6de2fe07ae54dbe3d6f9545b835c
Sunny <sunnybhowmick0310@gmail.com>
2026-04-04 16:31:13 +0530

    added forAllSystems, devshell, direnv

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     mnw.url = "github:Gerg-L/mnw";
   };
+
   outputs =
     {
       nixpkgs,
@@ -10,50 +11,64 @@
       self,
       ...
     }:
-    {
-      packages.x86_64-linux =
-        let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        in
-        {
-          default = mnw.lib.wrap pkgs {
-            neovim = pkgs.neovim-unwrapped;
-            initLua = builtins.readFile ./init.lua;
-            plugins = {
-              start = with pkgs.vimPlugins; [
-                lz-n
-                plenary-nvim
-                friendly-snippets
-                nvim-web-devicons
-                base16-nvim
-                oil-nvim
-              ];
-              opt = with pkgs.vimPlugins; [
-                telescope-nvim
-                blink-cmp
-                nvim-autopairs
-                lualine-nvim
-                nvim-lspconfig
-                conform-nvim
-                neogit
-                nvim-treesitter.withAllGrammars
-                (typst-preview-nvim.overrideAttrs {
-                  postPatch = ''
-                    substituteInPlace lua/typst-preview/config.lua \
-                      --replace-fail "['tinymist'] = nil" "['tinymist'] = 'tinymist'" \
-                      --replace-fail "['websocat'] = nil" "['websocat'] = 'websocat'"
-                  '';
-                })
-              ];
+    let
+      lib = nixpkgs.lib;
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-              dev.myconfig = {
-                pure = ./.;
-                impure = "/' .. vim.uv.cwd()  .. '/neovim-config";
-              };
+      forAllSystems =
+        function: lib.genAttrs supportedSystems (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = mnw.lib.wrap pkgs {
+          neovim = pkgs.neovim-unwrapped;
+          initLua = builtins.readFile ./init.lua;
+          plugins = {
+            start = with pkgs.vimPlugins; [
+              lz-n
+              plenary-nvim
+              friendly-snippets
+              nvim-web-devicons
+              base16-nvim
+              oil-nvim
+            ];
+            opt = with pkgs.vimPlugins; [
+              telescope-nvim
+              blink-cmp
+              nvim-autopairs
+              lualine-nvim
+              nvim-lspconfig
+              conform-nvim
+              neogit
+              nvim-treesitter.withAllGrammars
+              (typst-preview-nvim.overrideAttrs {
+                postPatch = ''
+                  substituteInPlace lua/typst-preview/config.lua \
+                    --replace-fail "['tinymist'] = nil" "['tinymist'] = 'tinymist'" \
+                    --replace-fail "['websocat'] = nil" "['websocat'] = 'websocat'"
+                '';
+              })
+            ];
+
+            dev.myconfig = {
+              pure = ./.;
+              impure = "/' .. vim.uv.cwd()  .. '/neovim-config";
             };
           };
-
-          dev = self.packages.x86_64-linux.default.devMode;
         };
+
+        dev = self.packages.${pkgs.stdenv.hostPlatform.system}.default.devMode;
+      });
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = lib.singleton self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+        };
+      });
     };
 }
