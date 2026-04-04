bf1e2116dc785dcd18ca29dc9a34fddae265bf27
Sunny <sunnybhowmick0310@gmail.com>
2026-04-04 17:50:24 +0530

    fasterer

--- init.lua
+++ init.lua
@@ -1,6 +1,51 @@
+vim.loader.enable()
+
+-- Diable builtins
+local vimg = vim.g
+
+local disabled_built_ins = {
+	"netrw",
+	"netrwPlugin",
+	"netrwSettings",
+	"netrwFileHandlers",
+	"gzip",
+	"zip",
+	"zipPlugin",
+	"tar",
+	"tarPlugin",
+	"getscript",
+	"getscriptPlugin",
+	"vimball",
+	"vimballPlugin",
+	"2html_plugin",
+	"logipat",
+	"rrhelper",
+	"spellfile_plugin",
+	"matchit",
+	"matchparen",
+	"editorconfig",
+	"man",
+	"osc52",
+	"shada",
+}
+
+for _, plugin in ipairs(disabled_built_ins) do
+	vimg["loaded_" .. plugin] = 1
+end
+
 -- Set the leader key to the spacebar
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vimg.mapleader = " "
+vimg.maplocalleader = " "
+vimg.loaded_perl_provider = 0
+vimg.loaded_ruby_provider = 0
+vimg.loaded_node_provider = 0
+vimg.loaded_python3_provider = 0
+vimg.clipboard = {
+	name = "wl-utils",
+	copy = { ["+"] = "wl-copy", ["*"] = "wl-copy" },
+	paste = { ["+"] = "wl-paste", ["*"] = "wl-paste" },
+	cache_enabled = 1,
+}
 
 local opt = vim.opt
 
@@ -40,44 +85,21 @@ opt.shadafile = "NONE"
 
 vim.o.winborder = "rounded"
 
-local disabled_built_ins = {
-	"netrw",
-	"netrwPlugin",
-	"netrwSettings",
-	"netrwFileHandlers",
-	"gzip",
-	"zip",
-	"zipPlugin",
-	"tar",
-	"tarPlugin",
-	"getscript",
-	"getscriptPlugin",
-	"vimball",
-	"vimballPlugin",
-	"2html_plugin",
-	"logipat",
-	"rrhelper",
-	"spellfile_plugin",
-	"matchit",
-}
-
-for _, plugin in ipairs(disabled_built_ins) do
-	vim.g["loaded_" .. plugin] = 1
-end
-
-local theme_file = os.getenv("HOME") .. "/.cache/matugen/matugen.lua"
+local theme_file = vim.fn.expand("~/.cache/matugen/matugen.lua")
 
 local function apply_matugen_theme()
-	package.loaded[theme_file] = nil
-	local colors = dofile(theme_file)
-	require("base16-colorscheme").setup(colors)
+	local ok, colors = pcall(dofile, theme_file)
+	if ok then
+		require("base16-colorscheme").setup(colors)
+	else
+		vim.cmd.colorscheme("base16-catppuccin-mocha")
+	end
 end
 
-if vim.fn.filereadable(theme_file) == 1 then
-	apply_matugen_theme()
-else
-	vim.cmd("colorscheme base16-catppuccin-mocha")
-end
+vim.api.nvim_create_autocmd("UIEnter", {
+	once = true,
+	callback = apply_matugen_theme,
+})
 
 local signal = vim.uv.new_signal()
 signal:start(
@@ -90,5 +112,17 @@ signal:start(
 	end)
 )
 
-require("oil").setup()
-require("lz.n").load("lazy")
+local target = vim.fn.argv(0)
+local stat = target and vim.uv.fs_stat(target)
+if stat and stat.type == "directory" then
+	vim.api.nvim_create_autocmd("VimEnter", {
+		once = true,
+		callback = function()
+			require("oil").setup()
+		end,
+	})
+end
+
+vim.schedule(function()
+	require("lz.n").load("lazy")
+end)
