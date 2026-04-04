71fe6ff0ebf4cd1cedd3a5f183c5575dece083fe
Sunny <sunnybhowmick0310@gmail.com>
2026-04-04 18:08:56 +0530

    fix typst preview (hopefully)

--- lua/lazy/typst-preview.lua
+++ lua/lazy/typst-preview.lua
@@ -2,7 +2,7 @@ return {
 	"typst-preview.nvim",
 	ft = "typst",
 	after = function()
-		require("typst-preview.nvim").setup({
+		require("typst-preview").setup({
 			invert_colors = "auto",
 			dependencies_bin = {
 				["tinymist"] = "tinymist",
