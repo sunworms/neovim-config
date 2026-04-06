06701efd4f5bec2eca4c619891358da2c82bd23d
Sunny <sunnybhowmick0310@gmail.com>
2026-04-06 10:15:22 +0530

    edit treesitter config

--- lua/lazy/extra.lua
+++ lua/lazy/extra.lua
@@ -10,14 +10,13 @@ return {
 		"nvim-treesitter",
 		event = "BufReadPost",
 		after = function()
-			require("nvim-treesitter").setup({
-				highlight = {
-    			enable = true,
-  			},
-				indent = {
-    			enable = true,
-  			},
+			vim.api.nvim_create_autocmd('FileType', {
+  		pattern = { '<filetype>' },
+  			callback = function() vim.treesitter.start() end,
 			})
+			vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+			vim.wo[0][0].foldmethod = 'expr'
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 		end,
 	},
 }
