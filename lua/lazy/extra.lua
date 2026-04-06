859ba1c926d386dee6746fed918cd032a112a7b6
Sunny <sunnybhowmick0310@gmail.com>
2026-04-06 10:26:21 +0530

    enable unfold by default

--- lua/lazy/extra.lua
+++ lua/lazy/extra.lua
@@ -10,13 +10,16 @@ return {
 		"nvim-treesitter",
 		event = "BufReadPost",
 		after = function()
-			vim.api.nvim_create_autocmd('FileType', {
-  		pattern = { '<filetype>' },
-  			callback = function() vim.treesitter.start() end,
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "<filetype>" },
+				callback = function()
+					vim.treesitter.start()
+				end,
 			})
-			vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-			vim.wo[0][0].foldmethod = 'expr'
+			vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+			vim.wo[0][0].foldmethod = "expr"
 			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			vim.opt.foldenable = false
 		end,
 	},
 }
