b68d9268f403e6335f43f3202d68d71013d48fce
Sunny <sunnybhowmick0310@gmail.com>
2026-07-19 18:48:44 +0530

    changed org event, made keybinds for today's journal and todo

--- nvim/lua/lazy/orgmode.lua
+++ nvim/lua/lazy/orgmode.lua
@@ -1,6 +1,6 @@
 return {
 	"orgmode",
-	ft = "org",
+	event = "DeferredUIEnter",
 	after = function()
 		require("orgmode").setup({
 			org_agenda_files = "~/Documents/gdrive/org/**/*",
@@ -9,5 +9,32 @@ return {
 		})
 
 		vim.lsp.enable("org")
+
+		local function open_today_journal()
+			local journal_dir = vim.fn.expand("~/Documents/gdrive/org/journal")
+			local filename = os.date("%Y-%m-%d") .. ".org"
+			local filepath = journal_dir .. "/" .. filename
+
+			vim.fn.mkdir(journal_dir, "p")
+
+			if vim.fn.filereadable(filepath) == 0 then
+				vim.fn.writefile({
+					"#+TITLE: " .. os.date("%Y-%m-%d"),
+					"",
+				}, filepath)
+			end
+
+			vim.cmd("edit " .. vim.fn.fnameescape(filepath))
+		end
+
+		vim.keymap.set("n", "<leader>oj", open_today_journal, {
+			desc = "Open today's journal",
+		})
+
+		vim.keymap.set("n", "<leader>ot", function()
+			vim.cmd("edit ~/Documents/gdrive/org/todo.org")
+		end, {
+			desc = "Open TODO file",
+		})
 	end,
 }
