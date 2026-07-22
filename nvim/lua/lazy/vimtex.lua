d046e1a4397dc9944faeb28feff4d09ba594589e
Sunny <sunnybhowmick0310@gmail.com>
2026-07-22 18:17:06 +0530

    change vimtex viewer

--- nvim/lua/lazy/vimtex.lua
+++ nvim/lua/lazy/vimtex.lua
@@ -2,6 +2,7 @@ return {
 	"vimtex",
 	ft = "tex",
 	after = function()
-		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_view_general_viewer = "qpdfview"
+		vim.g.vimtex_view_general_options = "--unique @pdf#src:@tex:@line:@col"
 	end,
 }
