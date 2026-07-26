af97b5bec1812ea4a9e78017cf908cb7c2b95c87
Sunny <sunnybhowmick0310@gmail.com>
2026-07-26 23:59:11 +0530

    going back to zathura

--- nvim/lua/lazy/vimtex.lua
+++ nvim/lua/lazy/vimtex.lua
@@ -2,7 +2,6 @@ return {
 	"vimtex",
 	ft = "tex",
 	after = function()
-		vim.g.vimtex_view_general_viewer = "qpdfview"
-		vim.g.vimtex_view_general_options = "--unique @pdf#src:@tex:@line:@col"
+		vim.g.vimtex_view_method = "zathura"
 	end,
 }
