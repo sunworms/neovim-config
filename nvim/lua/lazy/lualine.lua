df966afae5f641b56c08e033e532a694ca454490
Sunny <sunnybhowmick0310@gmail.com>
2026-07-29 22:28:45 +0530

    created lualine autocmd

--- nvim/lua/lazy/lualine.lua
+++ nvim/lua/lazy/lualine.lua
@@ -5,5 +5,13 @@ return {
 		require("lualine").setup({
 			options = { theme = require("matugen").lualine() },
 		})
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "MatugenReloaded",
+			callback = function()
+				require("lualine").setup({
+					options = { theme = require("matugen").lualine() },
+				})
+			end,
+		})
 	end,
 }
