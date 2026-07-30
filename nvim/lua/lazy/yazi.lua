889887166a424680f3de1be0b7361fef11ed4a6c
Sunny <sunnybhowmick0310@gmail.com>
2026-07-30 18:35:58 +0530

    managed to make yazi open in directories

--- nvim/lua/lazy/yazi.lua
+++ nvim/lua/lazy/yazi.lua
@@ -1,12 +1,24 @@
 return {
 	"yazi.nvim",
-	cmd = "Yazi",
+	event = "DeferredUIEnter",
 	after = function()
 		require("yazi").setup({
 			open_for_directories = true,
 			keymaps = {
 				show_help = "<f1>",
 			},
+		})
+
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function()
+				local arg = vim.fn.argv(0)
+				if arg == "" then
+					return
+				end
+				if vim.fn.isdirectory(arg) == 1 then
+					vim.cmd("Yazi " .. vim.fn.fnameescape(arg))
+				end
+			end,
 		})
 	end,
 	keys = {
