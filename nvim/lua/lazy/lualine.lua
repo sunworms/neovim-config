015148a50aad6cad3a60efa311d37727def3cafc
Sunny <sunnybhowmick0310@gmail.com>
2026-07-30 21:16:49 +0530

    made lualine conditional

--- nvim/lua/lazy/lualine.lua
+++ nvim/lua/lazy/lualine.lua
@@ -2,9 +2,24 @@ return {
 	"lualine.nvim",
 	event = "DeferredUIEnter",
 	after = function()
-		vim.opt.rtp:append("~/.config/nvim")
-		require("lualine").setup({
-			options = { theme = "base46-matugen" },
-		})
+		local uv = vim.uv or vim.loop
+		local function exists(path)
+			return uv.fs_stat(path) ~= nil
+		end
+
+		local colors = vim.fn.expand("~/.config/nvim/lua/lualine/themes/base46-matugen.lua")
+
+		local has_colors = exists(colors)
+
+		if has_colors then
+			vim.opt.rtp:append("~/.config/nvim")
+			require("lualine").setup({
+				options = { theme = "base46-matugen" },
+			})
+		else
+			require("lualine").setup({
+				options = { theme = "base46-catppuccin" },
+			})
+		end
 	end,
 }
