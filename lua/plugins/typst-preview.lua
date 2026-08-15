465f0205f701680bc39eadd9cc0c3a1f5d9a57ca
Sunny <sunnybhowmick0310@gmail.com>
2026-08-15 11:57:46 +0530

    remove build from typst-preview

--- lua/plugins/typst-preview.lua
+++ lua/plugins/typst-preview.lua
@@ -1,9 +1,6 @@
 return {
 	"chomosuke/typst-preview.nvim",
 	ft = "typst",
-	build = function()
-		require("typst-preview").update()
-	end,
 	config = function()
 		require("typst-preview").setup({
 			invert_colors = "auto",
