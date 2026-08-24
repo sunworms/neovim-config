65eee558f5d001dd434dad531365169a51d9ce60
Sunny <sunnybhowmick0310@gmail.com>
2026-08-24 19:47:09 +0530

    back to nvfetcher

--- default.nix
+++ default.nix
@@ -1,11 +1,16 @@
 {pkgs ? null}: let
-  inputs = import ./npins;
+  inputs = import ./_sources/generated.nix {
+    fetchurl = null;
+    fetchgit = null;
+    fetchFromGitHub = null;
+    dockerTools = null;
+  };
 
   finalPkgs =
     if pkgs != null
     then pkgs
     else
-      import inputs.nixpkgs {
+      import inputs.nixpkgs.src {
         config.allowUnfree = true;
       };
 in
