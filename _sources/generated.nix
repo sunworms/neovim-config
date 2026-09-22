f19ce2c386169a48cf51cab44c135cf124fe9400
Sunny <sunnybhowmick0310@gmail.com>
2026-09-22 15:06:43 +0530

    update nixpkgs

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -17,11 +17,11 @@
   };
   nixpkgs = {
     pname = "nixpkgs";
-    version = "20b1ddd1aa5ace70c9468305030aa4f9ef79671b";
+    version = "44a91898084f46797b5fac650c7e8c9ac38c43d4";
     src = fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/20b1ddd1aa5ace70c9468305030aa4f9ef79671b.tar.gz";
-      sha256 = "sha256-B44WL6h0XoLjJ41bUPJk0X5SDinLCII//6EcBLXKiJ0=";
+      url = "https://github.com/NixOS/nixpkgs/archive/44a91898084f46797b5fac650c7e8c9ac38c43d4.tar.gz";
+      sha256 = "sha256-Ft/BRnIqw1MywFoXydKobjjWmDFgDdYtSpJliE8+yUw=";
     };
-    date = "2026-09-19";
+    date = "2026-09-20";
   };
 }
